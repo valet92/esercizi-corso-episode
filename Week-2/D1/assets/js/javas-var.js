8b7f36f66a98c4cb5738cbbfd1d80a050bf8f87a
valet92 <56122244+valet92@users.noreply.github.com>
2023-02-08 17:06:33 +0100

    Delete Week-2 directory

--- Week-2/D1/assets/js/javas-var.js
+++
@@ -1,6 +0,0 @@
-var $pet = 'criceto';
-console.log($pet); // 5
-var _pet = 'leone';
-console.log(_pet); // 6
-var _pet2 = 'panterona';
-console.log(_pet2); // 7
--- Week-2/D1/esercizio corretto/EsercizioG1/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-document.write('<div id="container">output scritto da Javascript</div>'); //4
-
-document.getElementById('titolo').innerHTML = 'Iniziamo la nostra pratica'; //3
-
-alert('Usa il pop up per dare dei messaggi'); //1
-window.alert('Sto studiando JS');//2
